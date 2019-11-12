9165116c76484fbc68b2ffba17e912a3a6ddb00b
wangning <a>
2019-11-12 14:09:48 +0800

    123456

---
+++ src/main/java/com/test/demo/user/comtroller/HelloController.java
@@ -0,0 +1,12 @@
+package com.test.demo.user.comtroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(value="/hello")
+    public Object hello() {
+        return "Hello Mango!";
+    }
+}
---
+++ src/main/java/com/test/demo/user/comtroller/TestJDK8.java
@@ -0,0 +1,7 @@
+package com.test.demo.user.comtroller;
+
+public class TestJDK8 {
+    public static void main(String[] args) {
+
+    }
+}
