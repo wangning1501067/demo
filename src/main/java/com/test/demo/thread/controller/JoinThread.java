ba199c864b6f39a595c19816ccf4cd666a611740
wangning <wangningning@beyondsoft.com>
2020-04-20 15:49:05 +0800

    添加线程测试方法

--- src/main/java/com/test/demo/thread/controller/JoinThread.java
+++ src/main/java/com/test/demo/thread/controller/JoinThread.java
@@ -1,35 +1,30 @@
 package com.test.demo.thread.controller;
 
-public   class  JoinThread  extends  Thread
-{
-    public   static volatile int  n  =   0 ;
-    public   void  run()
-    {
-        for  ( int  i  =   0 ; i  <   10 ; i ++ )
-            try
-            {
-                n  =  n  +   1 ;
-                sleep( 3 );  //  为了使运行结果更随机，延迟3毫秒
+public class JoinThread extends Thread {
+    public static volatile int n = 0;
 
-            }
-            catch  (Exception e)
-            {
+    public void run() {
+        for (int i = 0; i < 10; i++)
+            try {
+                n = n + 1;
+                sleep(3);  //  为了使运行结果更随机，延迟3毫秒
+
+            } catch (Exception e) {
             }
     }
 
-    public   static   void  main(String[] args)  throws  Exception
-    {
+    public static void main(String[] args) throws Exception {
 
-        Thread threads[]  =   new  Thread[ 100 ];
-        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+        Thread threads[] = new Thread[100];
+        for (int i = 0; i < threads.length; i++)
             //  建立100个线程
-            threads[i]  =   new  JoinThread();
-        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+            threads[i] = new JoinThread();
+        for (int i = 0; i < threads.length; i++)
             //  运行刚才建立的100个线程
             threads[i].start();
-        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+        for (int i = 0; i < threads.length; i++)
             //  100个线程都执行完后继续
             threads[i].join();
-        System.out.println( " n= "   +  JoinThread.n);
+        System.out.println(" n= " + JoinThread.n);
     }
 }
--- src/main/java/com/test/demo/thread/controller/JoinThread1.java
+++ src/main/java/com/test/demo/thread/controller/JoinThread1.java
@@ -1,41 +1,35 @@
 package com.test.demo.thread.controller;
 
-public   class  JoinThread1  extends  Thread
-{
-    public   static int  n  =   0 ;
+public class JoinThread1 extends Thread {
+    public static int n = 0;
 
-    public static   synchronized   void  inc()
-    {
-        n ++ ;
+    public static synchronized void inc() {
+        n++;
     }
-    public   void  run()
-    {
-        for  ( int  i  =   0 ; i  <   10 ; i ++ )
-            try
-            {
+
+    public void run() {
+        for (int i = 0; i < 10; i++)
+            try {
                 inc();  //  n = n + 1 改成了 inc();
-                sleep( 3 );  //  为了使运行结果更随机，延迟3毫秒
+                sleep(3);  //  为了使运行结果更随机，延迟3毫秒
 
-            }
-            catch  (Exception e)
-            {
+            } catch (Exception e) {
             }
     }
 
 
-    public   static   void  main(String[] args)  throws  Exception
-    {
+    public static void main(String[] args) throws Exception {
 
-        Thread threads[]  =   new  Thread[ 100 ];
-        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+        Thread threads[] = new Thread[100];
+        for (int i = 0; i < threads.length; i++)
             //  建立100个线程
-            threads[i]  =   new  JoinThread();
-        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+            threads[i] = new JoinThread();
+        for (int i = 0; i < threads.length; i++)
             //  运行刚才建立的100个线程
             threads[i].start();
-        for  ( int  i  =   0 ; i  <  threads.length; i ++ )
+        for (int i = 0; i < threads.length; i++)
             //  100个线程都执行完后继续
             threads[i].join();
-        System.out.println( " n= "   +  JoinThread.n);
+        System.out.println(" n= " + JoinThread.n);
     }
 }
