ad2984f93eb6be15eb89c0c7d56416890c77b20b
wangning <wangningning@beyondsoft.com>
2020-08-07 15:38:17 +0800

    添加mq和bean销毁

--- src/main/java/com/test/demo/zhuru/test/ZhuRuTest2.java
+++ src/main/java/com/test/demo/zhuru/test/ZhuRuTest2.java
@@ -3,6 +3,6 @@ package com.test.demo.zhuru.test;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Primary
+//@Primary //Primary可以理解为默认优先选择,不可以同时设置多个,内部实质是设置BeanDefinition的primary属性
 public class ZhuRuTest2 extends ZhuRuTest {
 }
